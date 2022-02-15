4587f1d036961504898a876b86f70b69a56ee3ac
Solipsis <the.solipsis.project@gmail.com>
2022-02-14 16:04:42 -0800

    Add query helper function to graph common functions.

--- graph/common.ts
+++ graph/common.ts
@@ -1,6 +1,7 @@
 import { Algebra } from 'sparqljs';
 import { CID } from "ipfs-http-client";
-import { Graph } from 'sparql-engine';
+import { Bindings, Graph, HashMapDataset, PipelineStage, PlanBuilder } from 'sparql-engine';
+import { QueryOutput } from 'sparql-engine/dist/engine/plan-builder';
 
 export type IPLDValue = string|number|boolean|IPLD|Array<IPLDValue>|CID;
 
@@ -11,10 +12,47 @@ export interface IPLD {
 }
 
 export interface LinkedDataGraph extends Graph {
-    load(dbPath : string) : Promise<void>;
-    save(dbPath : string) : Promise<void>;
+    count(pattern? : Triple) : Promise<number>;
+    forEach(consumer : (pattern : Triple) => void) : Promise<void>;
+
     putIPLD(cid : CID, dag : IPLD) : Promise<void>;
     getIPLD(cid : CID, path : string) : Promise<IPLD>;
+
+    load(dbPath : string) : Promise<void>;
+    save(dbPath : string) : Promise<void>;
 }
+
+export function makeTriple(subject: string, predicate: string, object: string) {
+    return {subject, predicate, object};
+}
+
+        // TODO:
+        // -learn SPARQL
+        // -read documentation for sparql-engine
+        // -figure out what's going on with "start" and "end"
+
+export function resolveQuery(graph : LinkedDataGraph, query : string) : Promise<Array<any>> {
+    const IRI = 'http://example.org#graph-a'
+       
+    const dataset = new HashMapDataset(IRI, graph)
+       
+  // Creates a plan builder for the RDF dataset
+  const builder = new PlanBuilder(dataset)
+
+  // Get an iterator to evaluate the query
+  const iterator = builder.build(query) as PipelineStage<QueryOutput>;
+
+  // Read results
+  return new Promise<Array<any>>((resolve, reject) => {
+    var results = [];
+    iterator.subscribe(
+        bindings => {
+            results.push((bindings as Bindings).toObject());
+        },
+        err => reject(err),
+        () => resolve(results)
+      );
+  })
+} 
 
 export type Triple = Algebra.TripleObject;
