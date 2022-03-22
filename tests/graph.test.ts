f0305dee9059705b0ca1c85990f4133f68ad3d43
Solipsis <the.solipsis.project@gmail.com>
2022-03-22 13:47:21 -0700

    Add tests

--- tests/graph.test.ts
+++ tests/graph.test.ts
@@ -10,13 +10,15 @@ import { resolveQuery } from '../graph/common'
 
 const tempContainingDir = `./temp/`
 
-async function clearTempFiles() : Promise<void> {
+async function clearTempFiles(createTestDir : boolean) : Promise<void> {
     return new Promise<void>((resolve, reject) => {
         fs.rm(tempContainingDir, { recursive : true, force : true }, (err) => {
             if (err) {
                 reject(err);
             } else {
-                fs.mkdirSync(tempContainingDir);
+                if (createTestDir) {
+                    fs.mkdirSync(tempContainingDir);
+                }
                 resolve();
             }
         });
@@ -24,7 +26,11 @@ async function clearTempFiles() : Promise<void> {
 }
 
 beforeAll(async () => {
-    await clearTempFiles();
+    await clearTempFiles(true);
+});
+
+afterAll(async () => {
+    await clearTempFiles(false);
 });
 
 async function withTempDir(dbPath : string, callback) {
@@ -34,13 +40,11 @@ async function withTempDir(dbPath : string, callback) {
 }
 
 describe.each([ N3Graph, LevelRDFGraph])("Graph tests %O", (GraphClass) => {
-    test('single insert', async () => {    
+    test('empty graph', async () => {    
         await withTempDir(tempContainingDir, async (tempDir) => {
             const dbPath = `${tempDir}/db`;
             const graph = new GraphClass(dbPath);
             expect(await graph.count()).toBe(0);
-            await graph.insert(makeTriple('a', 'b', 'c'));
-            expect(await graph.count()).toBe(1);
 
             const query = `
             SELECT ?s
@@ -48,11 +52,63 @@ describe.each([ N3Graph, LevelRDFGraph])("Graph tests %O", (GraphClass) => {
             ?s ?p ?o
             }`
             const results = await resolveQuery(graph, query)
-            expect(results).toEqual([{ '?s' : 'a' }]);
+            expect(results).toEqual([]);
         });
     });
 
-    test('multiple inserts', async () => {   
+    test('single insert', async () => {    
+        await withTempDir(tempContainingDir, async (tempDir) => {
+            const dbPath = `${tempDir}/db`;
+            const graph = new GraphClass(dbPath);
+            expect(await graph.count()).toBe(0);
+            console.log(await graph.getIPLD({ toString : () => ':a' }, ''));
+            await graph.insert(makeTriple(':a', ':b', ':c'));
+            expect(await graph.count()).toBe(1);
+            console.log(await graph.getIPLD({ toString : () => ':a' }, ''));
+
+            const query = `
+            SELECT ?s ?p ?o
+            WHERE {
+            ?s ?p ?o
+            }`
+            const results = await resolveQuery(graph, query)
+            expect(results).toEqual([{ '?s' : 'a', '?p' : 'b', '?o' : 'c' }]);
+        });
+    });
+
+    test.only('multiple inserts', async () => {   
+        await withTempDir(tempContainingDir, async (tempDir) => { 
+            const dbPath = `${tempDir}/db`;
+            const graph = new GraphClass(dbPath);
+            expect(await graph.count()).toBe(0);
+            await graph.insert(makeTriple('https://mizu.io/a', 'https://mizu.io/b', 'https://mizu.io/c'));
+            expect(await graph.count()).toBe(1);
+            console.log(await graph.getIPLD({ toString : () => 'https://mizu.io/a' }, ''));
+            await graph.insert(makeTriple('https://mizu.io/d', 'https://mizu.io/e', 'https://mizu.io/f'));
+            expect(await graph.count()).toBe(2);
+            console.log(await graph.getIPLD({ toString : () => 'https://mizu.io/a' }, ''));
+
+            const query = `
+            PREFIX MIZU: <https://mizu.io/>
+            SELECT ?s
+            WHERE {
+            ?s ?p ?o
+            }`
+            const results = await resolveQuery(graph, query)
+            expect(results).toEqual([{ '?s' : 'https://mizu.io/a' }, { '?s' : 'https://mizu.io/d'}]);
+
+            const query2 = `
+            PREFIX MIZU: <https://mizu.io/>
+            SELECT *
+            WHERE {
+            <MIZU:a> ?p ?o
+            }`
+            const results2 = await resolveQuery(graph, query2)
+            expect(results2).toEqual([{ '?p' : 'MIZU:b' , '?o' : 'MIZU:c'}]);
+        });
+    });
+
+    test('inserting linked data', async () => {   
         await withTempDir(tempContainingDir, async (tempDir) => { 
             const dbPath = `${tempDir}/db`;
             const graph = new GraphClass(dbPath);
