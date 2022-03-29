c49462792959c2136f482a8c93011b5b1fce6f9b
Solipsis <the.solipsis.project@gmail.com>
2022-03-28 18:00:21 -0700

    Add query command.

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import * as PublishCommand from './cli/publish'
+import * as QueryCommand from './cli/query'
 import { baseYargsInjector } from './cli/yargs'
 import { publishCommand } from './publish';
+import { queryCommand } from './query';
 
 const args = baseYargsInjector
     .addCommand(PublishCommand, publishCommand)
-    .yargs.strict().parse()
+    .addCommand(QueryCommand, queryCommand)
+    .yargsObject.strict().parse()
