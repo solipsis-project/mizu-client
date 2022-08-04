3de9750bd9ec5190325e7c609290f5ff8a83361f
Solipsis <the.solipsis.project@gmail.com>
2022-08-04 00:39:49 -0700

    Add multiple log levels and configuration minimum log level

--- logger.ts
+++ logger.ts
@@ -1,11 +1,48 @@
 import _ from 'lodash';
 
+enum LogLevel {
+    DEBUG,
+    VERBOSE,
+    INFO,
+    WARN,
+    ERROR
+}
+
+var minimumLogLevel: LogLevel = LogLevel.INFO;
+
+export function setMinimumLogLevel(newMinimumLogLevel: LogLevel) {
+    minimumLogLevel = newMinimumLogLevel;
+}
+
 type LogOptions = {
     separator?: string;
     terminator?: string;
 }
 
 export function debug(generator: (logger: (...log: unknown[]) => unknown) => unknown, options: LogOptions = {}) {
+    log(LogLevel.DEBUG, generator);
+}
+
+export function verbose(generator: (logger: (...log: unknown[]) => unknown) => unknown, options: LogOptions = {}) {
+    log(LogLevel.VERBOSE, generator);
+}
+
+export function info(generator: (logger: (...log: unknown[]) => unknown) => unknown, options: LogOptions = {}) {
+    log(LogLevel.INFO, generator);
+}
+
+export function warn(generator: (logger: (...log: unknown[]) => unknown) => unknown, options: LogOptions = {}) {
+    log(LogLevel.WARN, generator);
+}
+
+export function error(generator: (logger: (...log: unknown[]) => unknown) => unknown, options: LogOptions = {}) {
+    log(LogLevel.ERROR, generator);
+}
+
+export function log(logLevel: LogLevel, generator: (logger: (...log: unknown[]) => unknown) => unknown, options: LogOptions = {}) {
+    if (logLevel < minimumLogLevel) {
+        return;
+    }
     const separator = options.separator || ' ';
     const terminator = options.terminator || '\n';
 
