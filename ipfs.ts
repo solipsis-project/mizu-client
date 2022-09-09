f5f11ee04475c843d3f61a89f479a7af12a50474
Solipsis <the.solipsis.project@gmail.com>
2022-09-09 16:37:18 -0700

    Create provider of IPFs interface, based on config flags.

--- ipfs.ts
+++ ipfs.ts
@@ -1,11 +1,12 @@
-/*import { IPFSMode, IPFSOptions } from './cli/options.js'
+import { IPFSMode, IPFSOptions } from './cli/options.js'
 import * as IpfsCore from 'ipfs-core'
 import * as IpfsHttp from 'ipfs-http-client'
+// import * as IPFSFull from 'ipfs'
 import type {IPFS} from 'ipfs-core-types'
 
-export async function create(options: IPFSOptions): Promise<any> {
+export async function create(options: IPFSOptions): Promise<IPFS> {
     if (options.type == IPFSMode.Internal) {
         return await IpfsCore.create();
     }
     return IpfsHttp.create({url: options.url});
-}*/
+}
