61c3c8e1d54f2f870b015f4b2bb63b8662df6041
Solipsis <the.solipsis.project@gmail.com>
2022-09-17 18:26:41 -0700

    Avoid depending on ipfs libraries for now.

--- ipfs.ts
+++ ipfs.ts
@@ -1,14 +1,17 @@
 import { IPFSMode, IPFSOptions } from './cli/options.js'
-import * as IpfsCore from 'ipfs-core'
-import * as IpfsHttp from 'ipfs-http-client'
-// import * as IPFSFull from 'ipfs'
-import type {IPFS as IpfsCoreType} from 'ipfs-core-types'
 
-export type IPFS = IpfsCoreType | IpfsHttp.IPFSHTTPClient;
+//import * as IpfsCore from 'ipfs-core'
+//import * as IpfsHttp from 'ipfs-http-client'
+// import * as IPFSFull from 'ipfs'
+//import type {IPFS as IpfsCoreType} from 'ipfs-core-types'
+
+//export type IPFS = IpfsCoreType | IpfsHttp.IPFSHTTPClient;
+
+export type IPFS = any;
 
 export async function create(options: IPFSOptions): Promise<IPFS> {
     if (options.type == IPFSMode.Internal) {
-        return await IpfsCore.create();
+        return await require('ipfs-core').create();
     }
-    return IpfsHttp.create({url: options.url});
+    return require('ipfs-http-client').create({url: options.url});
 }
