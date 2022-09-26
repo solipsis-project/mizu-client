9804e67041381c10b7891466e39db03252d77fee
Solipsis <the.solipsis.project@gmail.com>
2022-09-25 23:10:56 -0700

    Remove the check that the public key is a libp2p multicodec. (Also change to base58btc encoding)
    We use the format that is exported by libp2p/crypto.keys.marshallPublicKey. I'm not quite sure what format that is, but it doesn't appear to have a multicodec tag.

--- signer.ts
+++ signer.ts
@@ -1,10 +1,12 @@
 import { base32 } from 'multiformats/bases/base32'
+import { base58btc } from 'multiformats/bases/base58'
 // import varint from 'multiformats/varint.js'
 import { IPLD } from './graph/common.js';
 import * as crypto from '@libp2p/crypto'
 import * as dagCborCodec from '@ipld/dag-cbor'
 
 import varint from 'varint';
+import * as Logger from './logger.js';
 
 const LIBP2P_PUB = 0x72;
 
@@ -14,7 +16,7 @@ export function getSigner() : any {
 
 export async function verifySignature(dagWithoutSignatures: IPLD, encodedKey: string, encodedDigest: string) {
     // key is a multicodec containing a public key. By decoding it, we can determine the algorithm used.
-    const keyBuffer = base32.decode(encodedKey);
+    const keyBuffer = base58btc.decode(encodedKey);
     const keyType = varint.decode(keyBuffer);
     if (keyType != LIBP2P_PUB) {
         throw 'Signautre key must be a LibP2P public key multicodec.';
@@ -22,9 +24,10 @@ export async function verifySignature(dagWithoutSignatures: IPLD, encodedKey: st
     const key = crypto.keys.unmarshalPublicKey(keyBuffer);
 
     // Serialize the dag without signautres so that we can sign it.
+
     const dagCbor = dagCborCodec.encode(dagWithoutSignatures);
 
-    const digestBuffer = base32.decode(encodedDigest);
+    const digestBuffer = base58btc.decode(encodedDigest);
 
     if (!(await key.verify(dagCbor, digestBuffer))) {
         throw "Unable to verify key";
