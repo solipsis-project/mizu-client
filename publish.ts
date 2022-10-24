e49fc82c5fb110c72b2322962b03f714bd838880
Solipsis <the.solipsis.project@gmail.com>
2022-10-24 01:08:52 -0700

    Ensure that publishCommand always closes the ipfs client, even on errors.

--- publish.ts
+++ publish.ts
@@ -12,46 +12,52 @@ import { getSigner } from './signer.js';
 import { verify } from './verifier.js';
 import { multibase } from './multibase.js';
 
-export async function publishCommand(options: PublishOptions) {
+export async function publishCommand(options: PublishOptions): Promise<string> {
     Logger.setMinimumLogLevel(options.minimumLogLevel);
     const { inputOption, signingOption } = options;
     if (inputOption.type == InputType.Cid && signingOption.type != SigningType.None) {
         throw "Message signing is not allowed when input type is CID";
     }
     const ipfs_client = await createIpfs(options.ipfsOptions);
-    const dag = await getInput(inputOption, ipfs_client) as IPLD;
-    // TODO: confirm dag is a graph
-    verify(dag);
-    if (signingOption.type != SigningType.None) {
-        const signer = await getSigner(signingOption);
-        var signatures = dag[ReservedFields.SIGNATURES];
-        delete dag[ReservedFields.SIGNATURES];
-        if (_.isUndefined(signatures)) {
-            signatures = [];
+    try {
+        const dag = await getInput(inputOption, ipfs_client) as IPLD;
+        // TODO: confirm dag is a graph
+        await verify(dag);
+        if (signingOption.type != SigningType.None) {
+            const signer = await getSigner(signingOption);
+            var signatures = dag[ReservedFields.SIGNATURES];
+            delete dag[ReservedFields.SIGNATURES];
+            if (_.isUndefined(signatures)) {
+                signatures = [];
+            }
+            if (!_.isArray(signatures)) {
+                throw "Message has invalid $signatures field.";
+            }
+            const multicodecKey = signer.marshallPublicKey();
+            const digest = await signer.computeDigest(dag);
+            signatures.push({
+                [ReservedFields.SIGNATURES_KEY]: multibase.encode(multicodecKey),
+                [ReservedFields.SIGNATURES_DIGEST]: multibase.encode(digest),
+            })
+            dag[ReservedFields.SIGNATURES] = signatures;
         }
-        if (!_.isArray(signatures)) {
-            throw "Message has invalid $signatures field.";
+        const GraphClass = getStorage(options.storageType);
+        const graph = new GraphClass(options.databasePath);
+        const cid = (inputOption.type == InputType.Cid) ? CID.parse(inputOption.cid) : await ipfs_client.dag.put(dag);
+        ipfs_client.pin.add(cid);
+        Logger.debug((logger) => logger("message: ", dag));
+        if (!(dag instanceof Object)) {
+            throw "Published data is a primitive, not linked data.";
         }
-        const multicodecKey = signer.marshallPublicKey();
-        const digest = await signer.computeDigest(dag);
-        signatures.push({
-            [ReservedFields.SIGNATURES_KEY]:  multibase.encode(multicodecKey),
-            [ReservedFields.SIGNATURES_DIGEST]: multibase.encode(digest),
-        })
-        dag[ReservedFields.SIGNATURES] = signatures;
+        await publish(graph, cid, dag);
+        await graph.save(options.databasePath);
+        Logger.consoleLog(cid.toString());
+
+        return cid.toString();
     }
-    const GraphClass = getStorage(options.storageType);
-    const graph = new GraphClass(options.databasePath);
-    const cid = (inputOption.type == InputType.Cid) ? CID.parse(inputOption.cid) : await ipfs_client.dag.put(dag);
-    ipfs_client.pin.add(cid);
-    Logger.debug((logger) => logger("message: ", dag));
-    if (!(dag instanceof Object)) {
-        throw "Published data is a primitive, not linked data.";
+    finally {
+        await ipfs_client.stop();
     }
-    await publish(graph, cid, dag);
-    await graph.save(options.databasePath);
-    Logger.consoleLog(cid.toString());
-    ipfs_client.stop();
 }
 
 
