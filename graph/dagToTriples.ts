39adf6bf7a42800b3716f625a975a5ca456d881b
Solipsis <the.solipsis.project@gmail.com>
2022-10-24 01:12:26 -0700

    Refactor dagToTriples to correctly handle  array values (as per the data model)

--- graph/dagToTriples.ts
+++ graph/dagToTriples.ts
@@ -1,41 +1,46 @@
 // Convert the JSON DAG to a series of triples
 
+import _ from 'lodash';
 import { CID } from 'multiformats';
-import { IPFS } from '../ipfs.js'
-import { IPLDObject, IPLDValue, Triple } from "./common.js";
+import { IPLD, IPLDObject, IPLDValue, Triple } from "./common.js";
 
-async function* dagToTriplesInner(
-        root_path : string,
-        dag : IPLDObject,
-        follow_links : IPFS | false,
-        visited_subjects : Set<string>) : AsyncGenerator<Triple> {
-    if (visited_subjects.has(root_path))  {
-        return;
+async function* yieldTriple(
+    root_path: string,
+    key: string,
+    value: IPLDValue,
+    index: number | null = null
+) : AsyncGenerator<Triple> {
+    if (value instanceof CID) {
+        const child_path = value.toString();
+        yield { subject: root_path, predicate: key, object: child_path }
     }
-    visited_subjects.add(root_path);
-    for (const [key, value] of Object.entries(dag)) {
-        if (value instanceof CID) {
-            const child_path = value.toString();
-            yield { subject : root_path, predicate : key, object : child_path }
-            if (follow_links) {
-                const child_value = await follow_links.dag.get(value).then((dag) => dag.value);
-                yield* dagToTriplesInner(child_path, child_value, follow_links, visited_subjects);
-            }
-        }
-        else if (value instanceof Object) {
-            const child_path = `${root_path}/${key}`;
-            yield { subject : root_path, predicate : key, object : child_path }
-            yield* dagToTriplesInner(child_path, value, follow_links, visited_subjects);
-        } else {
-            yield { subject : root_path, predicate : key, object : value.toString() }
-        }
+    else if (value instanceof Object) {
+        const child_path = index != null ? `${root_path}/${key}/${index}` : `${root_path}/${key}`;
+        yield { subject: root_path, predicate: key, object: child_path }
+        yield* dagToTriples(child_path, value);
+    } else {
+        yield { subject: root_path, predicate: key, object: value.toString() }
     }
 }
 
 // TODO: Series of quads? CID is the graph name, path is the subject name?
 export default async function* dagToTriples(
-        root_path : string,
-        dag : IPLDObject,
-        follow_links : IPFS | false) : AsyncGenerator<Triple> {
-    yield* dagToTriplesInner(root_path, dag, follow_links, new Set<string>());
+    root_path: string,
+    dag: IPLD
+) : AsyncGenerator<Triple> {
+    for (const [key, value] of Object.entries(dag)) {
+        async function* inner(value: IPLDValue) {
+            
+        };
+        // In our data model, an array represents a multi-valued field. The same subject and predicate pair is reused for each element in the array.
+        if (_.isArray(value)) {
+            var index = 0;
+            for (const element of value) {
+                yield* yieldTriple(root_path, key, element, index);
+                index++;
+            }
+        } else {
+            yield* yieldTriple(root_path, key, value);
+        }
+    }
 }
