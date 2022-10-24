4f829611ece07284fcae259e7524dac728f25b96
Solipsis <the.solipsis.project@gmail.com>
2022-10-24 01:28:50 -0700

    Fix bug where verifySignature wasn't being awaited.

--- verifier.ts
+++ verifier.ts
@@ -9,14 +9,14 @@ import { verifySignature } from "./signer.js";
 import { CID } from 'multiformats';
 import * as Logger from "./logger.js";
 
-function getIterable(value: IPLDValue): Iterable<IPLDValue> {
+function getIterable(value: IPLDValue | Array<IPLDValue>): Iterable<IPLDValue> {
     if(_.isArray(value)) {
         return value;
     }
     return [value];
 }
 
-function verifySignatures(dagContainingSignatures : IPLD) {
+async function verifySignatures(dagContainingSignatures : IPLD) {
     const { [ReservedFields.SIGNATURES] : signatures, ...dagWithoutSignatures } = dagContainingSignatures;
     if (_.isUndefined(signatures)) {
         return;
@@ -37,24 +37,29 @@ function verifySignatures(dagContainingSignatures : IPLD) {
         if (!_.isString(digest)) {
             throw `Expected string for ${ReservedFields.SIGNATURES_DIGEST} field, got ${key}.`;
         }
-        verifySignature(dagWithoutSignatures, key, digest);
+        await verifySignature(dagWithoutSignatures, key, digest);
     }
 }
 
 // If the message fails to verify, an exception is thrown containing a user-readable message.
-export function verify(dag: IPLDValue) : void {
+export async function verify(dag: IPLDValue) : Promise<void> {
     if (!_.isObject(dag)) {
         return;
     }
     if (!_.isArray(dag)) {
-        verifySignatures(dag);
+        await verifySignatures(dag);
     }
     for (const [key, value] of Object.entries(dag)) {
         if (value instanceof CID) {
             throw "TODO: handle CID in verify";
         }
+        else if (_.isArray(value)) {
+            for (const element of value) {
+                await verify(element);
+            }
+        }
         else if (value instanceof Object) {
-            verify(value);
+            await verify(value);
         }
     }
     
