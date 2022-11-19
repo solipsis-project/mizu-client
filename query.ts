b4388aa7198f08805c4490ff0f6d8a5771edae6e
Solipsis <the.solipsis.project@gmail.com>
2022-11-18 23:27:20 -0800

    Remove noisy log statements and print result of query to stdout.

--- query.ts
+++ query.ts
@@ -6,13 +6,12 @@ import { QueryOptions } from './cli/query/options.js';
 import { CID } from 'multiformats'
 import createIpfs from './ipfs.js';
 import getStdin from 'get-stdin';
+import * as Logger from './logger.js';
 
 export async function queryCommand(options: QueryOptions) {
     const ipfs_client = await createIpfs(options.ipfsOptions);
     try {
         const query = await getStringInput(options.input, ipfs_client);
-
-        console.log(query);
 
         const GraphClass = getStorage(options.storageType);
         const graph = new GraphClass(options.databasePath);
@@ -20,9 +19,9 @@ export async function queryCommand(options: QueryOptions) {
 
         // TODO: Have mock persist between invocations.
         const results = await resolveQuery(graph, query);
-        results.forEach((result) => {
-            console.log(result);
-        });
+        Logger.consoleLog(JSON.stringify(results, null, 2));
+
+        return results;
     } finally {
         ipfs_client.stop();
     }
