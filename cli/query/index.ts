0eed4957a820b515cd8701e703f0963d5c7eaf7f
Solipsis <the.solipsis.project@gmail.com>
2022-12-11 10:55:20 -0800

    Add flags for query syntax (json-rql vs sparql), with json-rql as the default.

--- cli/query/index.ts
+++ cli/query/index.ts
@@ -1,21 +1,35 @@
 import { BaseCommand, addInputParameters, getStorageOptions, getInputOptions, getBaseCommandOptions } from "../yargs.js";
-import Flags from "./flags.js";
-import { BaseCommandOptions, QueryOptions } from "./options.js";
+import FLAGS from "./flags.js";
+import { BaseCommandOptions, QueryOptions, QuerySyntax } from "./options.js";
 
 export const command = 'query'
 export const desc = 'run a json-rql query on the database'
+
+function getQuerySyntax(argv) : QuerySyntax {
+    if (argv[FLAGS.SPARQL]) {
+        return QuerySyntax.Sparql;
+    }
+    if (argv[FLAGS.JSONRQL]) {
+        return QuerySyntax.JsonRql;
+    }
+    return QuerySyntax.JsonRql;
+}
 
 export function apply(yargs: BaseCommand, callback: (options: QueryOptions) => any) {
     return yargs.command(command,
         desc,
         (yargs: BaseCommand) => {
-            yargs = addInputParameters(yargs);
+            yargs = addInputParameters(yargs)
+                .boolean(FLAGS.JSONRQL)
+                .boolean(FLAGS.SPARQL)
+                .conflicts(FLAGS.JSONRQL, FLAGS.SPARQL);
             return yargs;
         },
         async (argv) => {
             const baseOptions: BaseCommandOptions = getBaseCommandOptions(argv);
             const queryOptions: QueryOptions = {
                 input: getInputOptions(argv),
+                syntax: getQuerySyntax(argv),
                 ...baseOptions
             }
             await callback(queryOptions);
--- cli/query/options.ts
+++ cli/query/options.ts
@@ -1,7 +1,10 @@
 import { BaseCommandOptions, InputOption } from "../options.js";
 
+export enum QuerySyntax { Sparql, JsonRql};
+
 export type QueryOptions = BaseCommandOptions & {
     input: InputOption,
+    syntax: QuerySyntax
 }
 
 export * from "../options.js"
