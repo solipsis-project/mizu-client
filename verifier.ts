1f7d071e3c46fb4754096d5032e394325394faf0
Solipsis <the.solipsis.project@gmail.com>
2022-12-17 15:32:49 -0800

    Reject messages containing unknown reserved fields.

--- verifier.ts
+++ verifier.ts
@@ -4,10 +4,11 @@
 
 import _ from "lodash";
 import { IPLD, IPLDObject, IPLDValue } from "./graph/common.js";
-import ReservedFields from "./reserved_fields.js";
+import {ReservedFields, ReservedFieldConstants} from "./reserved_fields.js";
 import { verifySignature } from "./signer.js";
 import { CID } from 'multiformats';
 import * as Logger from "./logger.js";
+import { VerificationError } from "./errors.js";
 
 function getIterable(value: IPLDValue | Array<IPLDValue>): Iterable<IPLDValue> {
     if(_.isArray(value)) {
@@ -17,25 +18,25 @@ function getIterable(value: IPLDValue | Array<IPLDValue>): Iterable<IPLDValue> {
 }
 
 async function verifySignatures(dagContainingSignatures : IPLD) {
-    const { [ReservedFields.SIGNATURES] : signatures, ...dagWithoutSignatures } = dagContainingSignatures;
+    const { [ReservedFieldConstants.SIGNATURES] : signatures, ...dagWithoutSignatures } = dagContainingSignatures;
     if (_.isUndefined(signatures)) {
         return;
     }
     for (const signature of getIterable(signatures)) {
-        const key = signature[ReservedFields.SIGNATURES_KEY];
+        const key = signature[ReservedFieldConstants.SIGNATURES_KEY];
         if (_.isUndefined(key)) {
-            throw `Missing ${ReservedFields.SIGNATURES_KEY} field.`;
+            throw new VerificationError(`Missing ${ReservedFieldConstants.SIGNATURES_KEY} field.`);
         }
         if (!_.isString(key)) {
-            throw `Expected string for ${ReservedFields.SIGNATURES_KEY} field, got ${key}.`;
+            throw new VerificationError(`Expected string for ${ReservedFieldConstants.SIGNATURES_KEY} field, got ${key}.`);
         }
         
-        const digest = signature[ReservedFields.SIGNATURES_DIGEST];
+        const digest = signature[ReservedFieldConstants.SIGNATURES_DIGEST];
         if (_.isUndefined(digest)) {
-            throw `Missing ${ReservedFields.SIGNATURES_DIGEST} field.`;
+            throw new VerificationError(`Missing ${ReservedFieldConstants.SIGNATURES_DIGEST} field.`);
         }
         if (!_.isString(digest)) {
-            throw `Expected string for ${ReservedFields.SIGNATURES_DIGEST} field, got ${key}.`;
+            throw new VerificationError(`Expected string for ${ReservedFieldConstants.SIGNATURES_DIGEST} field, got ${key}.`);
         }
         await verifySignature(dagWithoutSignatures, key, digest);
     }
@@ -50,6 +51,9 @@ export async function verify(dag: IPLDValue) : Promise<void> {
         await verifySignatures(dag);
     }
     for (const [key, value] of Object.entries(dag)) {
+        if (key.startsWith("$") && !key.startsWith("$$") && !ReservedFields.includes(key)) {
+            throw new VerificationError(`Unknown reserved field: ${key}`);
+        }
         if (value instanceof CID) {
             throw "TODO: handle CID in verify";
         }
