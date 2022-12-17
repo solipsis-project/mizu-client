b0c67483294b5a88e6257ee0233d570bb90b8b60
Solipsis <the.solipsis.project@gmail.com>
2022-12-16 16:40:12 -0800

    Allow keygen script to be invoked directly or called as a command within mizu.

--- scripts/keygen.ts
+++ scripts/keygen.ts
@@ -1,18 +1,22 @@
 import * as crypto from '@libp2p/crypto'
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers'
+import esMain from 'es-main';
 
-yargs(hideBin(process.argv)).command(
-    '$0 <password>',
-    '',
-    (yargs) => {
-        return yargs.positional('password', {
-            describe: 'Password to encrypt key with',
-            type: 'string',
-            default: ''
-        });
-    }, async (argv) => {
+export function builder(yargs) {
+    return yargs.positional('password', {
+        describe: 'Password to encrypt key with',
+        type: 'string',
+        default: ''
+    });
+}
+
+export async function handler(argv) {
     const key = await crypto.keys.generateKeyPair('RSA');
 
     console.log(await key.export(argv.password));
-}).strict().parse();
+}
+
+if (esMain(import.meta)) {
+    yargs(hideBin(process.argv)).command('$0 <password>', '', builder, handler).strict().parse();
+}
